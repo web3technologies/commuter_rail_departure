db1bfb632d22cc4245a26a2014a6b0d001a60d0f
zach <zach@web3technologies.io>
2024-02-06 15:41:57 -0600

    update static root

--- src/commuter_rail_departure/settings/prod.py
+++ src/commuter_rail_departure/settings/prod.py
@@ -8,3 +8,6 @@ CORS_ORIGIN_WHITELIST = [
     'https://commuter-rail-departure.vercel.app',
     "https://commuter-rail-departure-git-develop-web3technologies.vercel.app"
 ]
+
+MEDIA_ROOT = "/applications/commuter_rail_departure/"
+STATIC_ROOT = f"/applications/commuter_rail_departure/static/"
